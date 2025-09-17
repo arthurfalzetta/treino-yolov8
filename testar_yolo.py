ad8620b5755206e0890bad673f5048252d4dbf77
Enrico-Made <enrico.bernardi@madeinweb.com.br>
2025-09-17 09:25:27 -0300

    Update classes dataset

--- testar_yolo.py
+++ testar_yolo.py
@@ -15,16 +15,24 @@ for file in os.listdir(input_folder):
         results = model(path, save=True, project="resultados", name="deteccao", exists_ok=True)
 
         for r in results:
-            contagem = {"coluna": 0, "viga": 0, "laje": 0}
+            contagem = {"column": 0, "beam": 0, "slab": 0, "person": 0, "wall": 0, "crane": 0, "lift car": 0, "ring clurch": 0, "stair": 0, "trailer": 0, "upright": 0}
             for c in r.boxes.cls:
                 elem = model.names[int(c)]
                 contagem[elem] += 1
 
             dados.append({
                 "imagem": file,
-                "colunas_detectadas": contagem["coluna"],
-                "vigas_detectadas": contagem["viga"],
-                "lajes_detectadas": contagem["laje"]
+                "colunas_detectadas": contagem["column"],
+                "vigas_detectadas": contagem["beam"],
+                "lajes_detectadas": contagem["slab"],
+                "pessoas_detectadas": contagem["person"],
+                "paredes_detectadas": contagem["wall"],
+                "guindastes_detectados": contagem["crane"],
+                "elevadores_detectados": contagem["lift car"],
+                "escadas_detectadas": contagem["stair"],
+                "carretas_detectadas": contagem["trailer"],
+                "estruturas_verticais_detectadas": contagem["upright"],
+                "estruturas_anel_detectadas": contagem["ring clurch"]
             })
 
 df = pd.Dataframe(dados)
